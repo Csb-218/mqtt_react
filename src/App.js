df99e2118479eb2863d420303739c0579448a526
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2023-05-27 20:31:52 +0530

    Connected with mqtt client

--- src/App.js
+++ src/App.js
@@ -5,12 +5,43 @@ import axios from 'axios';
 import './App.css';
 import {useState,useEffect} from 'react';
 import {Routes,Route} from 'react-router-dom';
+import mqtt from 'mqtt-browser';
 
 
 function App() {
-
-  //Device cards 
+  
+//Device cards 
  const [deviceCards,setDeviceCards] = useState([]);
+
+ const [msg,setMsg] = useState({
+  datetime: '2023-05-27 00:00:00',
+  device_name: 'raspberryPi',
+  cpu:'...',
+  temperature:'...',
+  memory:'...'
+ });
+
+ const host = 'ws://3.109.62.128:8000/mqtt';
+ const options = {
+  // Clean session
+  clean: true,
+  keepalive: 0,
+  clientId: 'mqttjs_5c32dc7e',
+}
+
+useEffect(()=>{
+  const client = mqtt.connect(host, options);
+  client.subscribe('admin/heartbeat');
+  let note;
+  client.on("message", function (topic, message) {
+  note = JSON.parse(message.toString());
+  // Updates React state with message
+  setMsg(note);
+  console.log(note);
+}) 
+},[]);
+
+  
   //Fetching device cards
   useEffect(() => {
      
@@ -35,18 +66,15 @@ function App() {
      .then(res=> res.status===200?console.log('Device added'):null)
      .catch(error => alert(error.response.data.message));
    }
- 
 
   return (
    <>  
-      
-       <Routes>
-            <Route path='/' element={<MiniDrawer deviceCards={deviceCards} deleteDevice={deleteDevice} addDevice={addDevice} /> }/>
+         <div>
+         </div>
+         <Routes>
+            <Route path='/' element={<MiniDrawer deviceCards={deviceCards} deleteDevice={deleteDevice} addDevice={addDevice} msg={msg} /> }/>
             <Route path='/:id' element={<DeviceDetails/>}/>
        </Routes>
-
-
-   
    </>
   );
 }
